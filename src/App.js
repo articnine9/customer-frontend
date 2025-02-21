62f1293271f9aa25dfd0c75ff19183367b2f8c90
articnine9 <articnine9@gmail.com>
2025-02-21 10:53:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,8 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppRoutes from "./Routes/appRoutes";
 
-
-
-
-
- 
 function App() {
- return (
-            
-            <AppRoutes/>
-      
-  );
+  return <AppRoutes />;
 }
- 
+
 export default App;
-
-
-
